9678425f3bf00974e4987fa73840e94b4436e96c
Akash <akashkuskar969696@gmail.com>
2023-09-20 20:36:17 +0530

    Initial commit

---
+++ Database/scrip.js
@@ -0,0 +1,20 @@
+// <!-- javaScript -->
+// <!-- <script src="https://smtpjs.com/v3/smtp.js"></script>
+// <script>
+//   function sendEmail(){
+//     Email.send({
+//         Host : "smtp.elasticemail.com",
+//         Username : "akashkfreelancer@gmail.com",
+//         Password : "44F48C340A5BB52375E2EB11C43B1D4ECA70",
+//         To : 'topworldtours@gmail.com ',
+//         From : document.getElementById("email").value,
+//         Subject :"New contact Form Enquiry",
+//         Body : "Name : "+ document.getElementById("name").value
+//         + "<br> Email: "+ document.getElementById("email").value
+//         + "<br> Subject: "+ document.getElementById("subject").value
+//         + "<br> Message: "+ document.getElementById("message").value,
+//     }).then(
+//       message => alert("Message send successfully!")
+// );
+//   }
+// </script> -->
